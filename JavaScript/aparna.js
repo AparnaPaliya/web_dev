96ed2b834486609c673d2eac1e5b0d89f162f850
AparnaPaliya <116474313+AparnaPaliya@users.noreply.github.com>
2024-08-16 11:44:34 +0530

    javascript

---
+++ JavaScript/aparna.js
@@ -0,0 +1,24 @@
+var a = 78;
+var a = 65;
+var b = "Aparna";
+// var can be redeclare but let and const cant'. let can be updated but const cant' 
+console.log(a);
+console.log(b);
+console.log('Hello world');
+let Name = "Aparna Paliya";
+let Age = 24;
+Age = 25;
+Age = 26;
+ const Price = 99.99;
+Dream = 'IAS'
+x = null;
+y = undefined;
+console.log(Name);
+console.log(Age);
+console.log(Price);
+console.log(Dream);
+console.log(x);
+console.log(y)
+
+
+
---
+++ JavaScript/tut65.js
@@ -0,0 +1,9 @@
+console.log("hello");
+alert("Aparna Paliya");
+window.console.log("hello2");
+window.alert("hello");
+console.log(window);  // by this command in console you can see the doc. file of html DOM  in javascript//
+console.dir(window.document);
+console.log(document.body);
+console.dir(document.body);
+console.log(document.head);
